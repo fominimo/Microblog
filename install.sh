bf223c9f947e48a30191f65a31282d69673f7fc3
fominimo <fominimo@yandex.ru>
2020-11-03 20:19:36 +0300

    server inst

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [ $1 == '--help' ]; then
+if [ "${1:-""}" = '--help' -o "${1:-""}" = "" ]; then
     echo "usage: install.sh your-domain"
-    echo "./install.sh test.akhromlyuk.dev"
+    echo -e "1: введите желаемый адрес сайта\n2: введите порт\n3: Использовать ssl 'true' (опционально, по умолчанию true)"
+    echo "./install.sh test.akhromlyuk.dev 9001"
+    exit 0
 fi
 
 set -eu
 
+DOMAIN="${1:-"default_socket.akhromlyuk.dev"}"
+PORT="${2:-"9001"}"
+SSLON="${3:-"true"}"
+SERVER_IP="$(ip -4 a | grep "inet" | egrep "/24" | \
+ awk -F' ' '{ print $2 }' | rev | cut -c 4- | rev)"
+
 . ./installer/logs
+
+. ./installer/utils
 
 main() {
     to_action "[START] installer"
@@ -16,8 +26,11 @@ main() {
 
     python3_worker
     pip_worker
+    install_service
+    install_nginx
     success "SUCCESS"
     to_action "[END] installer"
+    _end "http://$DOMAIN" "$SERVER_IP:$PORT"
     return 0
 }
 
@@ -47,3 +60,4 @@ python3_worker() {
 
 main "$@"
 
+exit 0
